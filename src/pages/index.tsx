f1d168ae42372389dc5cd5c729de0e4940c99d45
Ching-Hsuan Huang <111054305@ntut.edu.tw>
2023-04-02 20:32:34 +0800

    add legend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,42 +9,69 @@ const App: React.FC = () => (
   <>
     <h2>人口數、戶數按戶別及性別統計</h2>
     <Space wrap>
-      年份：
-      <Select
-        defaultValue="lucy"
-        style={{ width: 120 }}
-        onChange={handleChange}
-        options={[
-          { value: 'jack', label: 'Jack' },
-          { value: 'lucy', label: 'Lucy' },
-          { value: 'Yiminghe', label: 'yiminghe' },
-          { value: 'disabled', label: 'Disabled', disabled: true },
-        ]}
-      />
-      縣市：
-      <Select
-        defaultValue="lucy"
-        style={{ width: 120 }}
-        onChange={handleChange}
-        options={[
-          { value: 'jack', label: 'Jack' },
-          { value: 'lucy', label: 'Lucy' },
-          { value: 'Yiminghe', label: 'yiminghe' },
-          { value: 'disabled', label: 'Disabled', disabled: true },
-        ]}
-      />
-      區：
-      <Select
-        defaultValue="lucy"
-        style={{ width: 120 }}
-        onChange={handleChange}
-        options={[
-          { value: 'jack', label: 'Jack' },
-          { value: 'lucy', label: 'Lucy' },
-          { value: 'Yiminghe', label: 'yiminghe' },
-          { value: 'disabled', label: 'Disabled', disabled: true },
-        ]}
-      />
+      <fieldset style={{ borderRadius: '6px' }}>
+        <legend>年份</legend>
+        <Select
+          defaultValue="lucy"
+          bordered={false}
+          style={{
+            width: 120,
+            marginTop: '-10px',
+            marginBottom: '-6px',
+            marginLeft: '-10px',
+            marginRight: '-10px',
+          }}
+          onChange={handleChange}
+          options={[
+            { value: 'jack', label: 'Jack' },
+            { value: 'lucy', label: 'Lucy' },
+            { value: 'Yiminghe', label: 'yiminghe' },
+            { value: 'disabled', label: 'Disabled', disabled: true },
+          ]}
+        />
+      </fieldset>
+      <fieldset style={{ borderRadius: '6px' }}>
+        <legend>縣市：</legend>
+        <Select
+          defaultValue="lucy"
+          bordered={false}
+          style={{
+            width: 120,
+            marginTop: '-10px',
+            marginBottom: '-6px',
+            marginLeft: '-10px',
+            marginRight: '-10px',
+          }}
+          onChange={handleChange}
+          options={[
+            { value: 'jack', label: 'Jack' },
+            { value: 'lucy', label: 'Lucy' },
+            { value: 'Yiminghe', label: 'yiminghe' },
+            { value: 'disabled', label: 'Disabled', disabled: true },
+          ]}
+        />
+      </fieldset>
+      <fieldset style={{ borderRadius: '6px' }}>
+        <legend>區</legend>
+        <Select
+          bordered={false}
+          defaultValue="lucy"
+          style={{
+            width: 120,
+            marginTop: '-10px',
+            marginBottom: '-6px',
+            marginLeft: '-10px',
+            marginRight: '-10px',
+          }}
+          onChange={handleChange}
+          options={[
+            { value: 'jack', label: 'Jack' },
+            { value: 'lucy', label: 'Lucy' },
+            { value: 'Yiminghe', label: 'yiminghe' },
+            { value: 'disabled', label: 'Disabled', disabled: true },
+          ]}
+        />
+      </fieldset>
       <Button type="primary">SUBMIT</Button>
     </Space>
   </>
