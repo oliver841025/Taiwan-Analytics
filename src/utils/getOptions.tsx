480adff867c6ab40abcf1f6de9896093e6c6fe5a
Ching-Hsuan Huang <111054305@ntut.edu.tw>
2023-04-10 17:41:18 +0800

    fix getOptions

--- src/utils/getOptions.tsx
+++ src/utils/getOptions.tsx
@@ -4,7 +4,7 @@ export default function getOptions() {
   const options = new Set();
   const result: any[] = [];
 
-  const getCityOptions = (records: []) => {
+  const getCityOptions = (records: any) => {
     records.forEach((item: any) => {
       !options.has(item.site_id.substring(0, 3))
         ? options.add(item.site_id.substring(0, 3))
@@ -19,6 +19,7 @@ export default function getOptions() {
   };
 
   const getDistrictOptions = (records: any, city: any) => {
+    if (!records) return;
     records.forEach((item: any) => {
       // 確認只有在 city 符合的情況下才會給相對應的區選項
       if (item.site_id.substring(0, 3) === city?.toString()) {
